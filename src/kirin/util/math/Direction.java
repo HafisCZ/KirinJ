89e5c31dbb07b2bacc71d631131807056e22ab37
Martin 'Hafis' Halfar <mar00m@seznam.cz>
2017-10-07 23:29:50 +0200

    V01.B005

--- src/kirin/util/math/Direction.java
+++ src/kirin/util/math/Direction.java
@@ -16,6 +16,41 @@ public enum Direction {
 		this.angle = angle;
 	}
 	
+	public static Direction getDirection(double angle) {
+		switch ((int) angle % 360) {
+			case 0:
+				return EAST;
+			case 45:
+				return NORTHEAST;
+			case 90:
+				return NORTH;
+			case 135:
+				return NORTHWEST;
+			case 180:
+				return WEST;
+			case 225:
+				return SOUTHWEST;
+			case 270:
+				return SOUTH;			
+			case 315:
+				return SOUTHEAST;
+			default:
+				return EAST;
+		}
+	}
+	
+	public static Direction getInverted(Direction direction) {
+		return getDirection(direction.getAngle() + 180);
+	}
+	
+	public static Direction getNextRight(Direction direction) {
+		return getDirection(direction.getAngle() + 270);
+	}
+	
+	public static Direction getNextLeft(Direction direction) {
+		return getDirection(direction.getAngle() + 90);
+	}
+	
 	/**
 	 * Get normalized vector from direction
 	 * @return Directional vector
